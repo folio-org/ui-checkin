149a115ed22815a9d344bfa95b35915a6c3f8755
Zak Burke <zburke@cornell.edu>
2017-10-12 06:20:45 -0400

    Use PropTypes, not React.PropTypes. Refs STRIPES-427

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types'; 
+import PropTypes from 'prop-types';
 import Route from 'react-router-dom/Route';
 import Switch from 'react-router-dom/Switch';
 import Scan from './Scan';
@@ -7,7 +7,7 @@ import Scan from './Scan';
 class CheckInRouting extends React.Component {
 
   static childContextTypes = {
-    history: React.PropTypes.object,
+    history: PropTypes.object,
   };
 
   static propTypes = {
