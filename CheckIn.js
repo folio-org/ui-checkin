a14024d6023d43b014e27dde1d9da6e546a10749
ADITYA MATUKUMALLI <aditya.matukumalli@gmail.com>
2018-03-28 16:06:58 -0400

    Uichkin 10 refinement (#22)
    * UICHKIN-10 scenario 1
    
    * fix lint errors/warns
    
    * add holdings to manifest
    
    * UICHKIN-1O scenario 2, added columns with link to loan details
    
    * Checkin Refinement - refs UICHKIN-10
    
    - Added the datepicker and timepicker components that represent the check-in date and time respectively
    - Added the info icon popup that displays the processed time and the actual time when the book was checked in
    - Each record now has details about the call number and  material types
    
    * Added a new function to util that returns date in UTC with inputs from date and timepicker
    
    * fix typo
    
    * Checkin page refinement, refs UICHKIN-10
    
    - Added the gear icon to the title column
    - fix lint
    
    * Refine the checkin page to handle timezone refs UIORG-55
    
    * pass timezone to formatdatetimepicker in util
    
    * add moment-timezone to package json refs UIORG-55

--- CheckIn.js
+++ CheckIn.js
@@ -2,20 +2,30 @@ import _ from 'lodash';
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Field, reduxForm } from 'redux-form';
-import Link from 'react-router-dom/Link';
 import Paneset from '@folio/stripes-components/lib/Paneset';
 import Pane from '@folio/stripes-components/lib/Pane';
 import Button from '@folio/stripes-components/lib/Button';
 import MultiColumnList from '@folio/stripes-components/lib/MultiColumnList';
-import { Row, Col } from '@folio/stripes-components/lib/LayoutGrid';
 import TextField from '@folio/stripes-components/lib/TextField';
+import Datepicker from '@folio/stripes-components/lib/Datepicker';
+import Timepicker from '@folio/stripes-components/lib/Timepicker';
+import IconButton from '@folio/stripes-components/lib/IconButton'; // eslint-disable-line import/no-extraneous-dependencies
+import Layout from '@folio/stripes-components/lib/Layout';
+import { Row, Col } from '@folio/stripes-components/lib/LayoutGrid';
+
 
 const propTypes = {
   scannedItems: PropTypes.arrayOf(PropTypes.object),
   handleSubmit: PropTypes.func.isRequired,
   submitting: PropTypes.bool,
+  pristine: PropTypes.bool,
   submithandler: PropTypes.func,
+  showInfo: PropTypes.func,
+  onSessionEnd: PropTypes.func,
+  renderActions: PropTypes.func,
+  stripes: PropTypes.object,
 };
+
 
 function CheckIn(props) {
   const containerStyle = {
@@ -27,25 +37,43 @@ function CheckIn(props) {
     position: 'absolute',
   };
 
-  const itemListFormatter = {
-    barcode: loan => `${_.get(loan, ['item', 'barcode'])}`,
-    title: loan => `${_.get(loan, ['item', 'title'])}`,
-    location: loan => `${_.get(loan, ['item', 'location', 'name'])}`,
-    patron: (loan) => {
-      const url = `/users/view/${_.get(loan, ['patron', 'id'])}`;
-      return <Link to={url}>{ _.get(loan, ['patron', 'personal', 'lastName']) }, { _.get(loan, ['patron', 'personal', 'firstName']) }</Link>;
-    },
-    'Date Loaned': loan => loan.loanDate.substr(0, 10),
-    'Date Returned': loan => loan.returnDate.substr(0, 10),
-    status: loan => `${_.get(loan, ['item', 'status', 'name'])}`,
-  };
-
   const {
     handleSubmit,
     submitting, // eslint-disable-line no-unused-vars
     submithandler,
+    onSessionEnd,
     scannedItems,
+    pristine,
+    showInfo,
+    renderActions,
+    stripes,
   } = props;
+
+  const itemListFormatter = {
+    'Time Returned': loan => (
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <div>{stripes.formatTime(`${_.get(loan, ['systemReturnDate'])}`)}</div>
+        <div key={loan.id}>{showInfo(loan)}</div>
+      </div>
+    ),
+    Title: (loan) => {
+      const title = `${_.get(loan, ['item', 'title'])}`;
+      const materialType = `${_.get(loan, ['item', 'materialType', 'name'])}`;
+      return `${title} (${materialType})`;
+    },
+    Barcode: loan => `${_.get(loan, ['item', 'barcode'])}`,
+    Location: loan => `${_.get(loan, ['item', 'location', 'name'])}`,
+    Status: loan => `${_.get(loan, ['item', 'status', 'name'])}`,
+    CallNumber: (loan) => {
+      const callNumber = `${_.get(loan, ['item', 'callNumber'])}`;
+      return callNumber !== 'undefined' ? callNumber : ' ';
+    },
+    ' ': loan => renderActions(loan),
+  };
+
+  const columnMapping = {
+    ' ': <IconButton style={{ marginLeft: '-6px' }} icon="gear" aria-label="action settings" />,
+  };
 
   return (
     <form onSubmit={handleSubmit(submithandler)}>
@@ -54,21 +82,53 @@ function CheckIn(props) {
           <Pane paneTitle="Scanned Items" defaultWidth="100%">
             <div style={{ width: '100%', maxWidth: '1280px', margin: 'auto' }}>
               <Row>
-                <Col xs={9}>
-                  <Field id="input-item-barcode" name="item.barcode" validationEnabled={false} placeholder="Enter Barcode" aria-label="Item ID" fullWidth component={TextField} />
+                <Col xs={9} sm={4}>
+                  <Layout className="marginTopLabelSpacer">
+                    <Field id="input-item-barcode" name="item.barcode" validationEnabled={false} rounded placeholder="Scan or enter barcode to check-in item" aria-label="Item ID" fullWidth component={TextField} />
+                  </Layout>
                 </Col>
-                <Col xs={3}>
-                  <Button id="clickable-add-item" buttonStyle="primary noRadius" fullWidth type="submit" disabled={submitting}>+ Add item</Button>
+                <Col xs={3} sm={1}>
+                  <Layout className="marginTopLabelSpacer">
+                    <Button id="clickable-add-item" buttonStyle="primary" fullWidth type="submit" disabled={pristine}>Enter</Button>
+                  </Layout>
+                </Col>
+                <Col xs={12} smOffset={2} sm={2}>
+                  <Field
+                    name="item.checkinDate"
+                    aria-label="checkin Date"
+                    label="Process as:"
+                    rounded
+                    component={Datepicker}
+                    passThroughValue="today"
+                  />
+                </Col>
+                <Col xs={12} sm={2}>
+                  <Field
+                    name="item.checkinTime"
+                    aria-label="checkin Time"
+                    label="Time returned:"
+                    rounded
+                    component={Timepicker}
+                    passThroughValue="now"
+                  />
+                </Col>
+                <Col xs={12} sm={1}>
+                  <Layout className="marginTopLabelSpacer">
+                    <Button id="clickable-end-session" buttonStyle="default" style={{ minWidth: '90px' }} fullWidth onClick={onSessionEnd}>End session</Button>
+                  </Layout>
                 </Col>
               </Row>
               <MultiColumnList
                 id="list-items-checked-in"
-                visibleColumns={['barcode', 'title', 'location', 'patron', 'Date Loaned', 'Date Returned', 'status']}
+                fullWidth
+                visibleColumns={['Time Returned', 'Title', 'Barcode', 'CallNumber', 'Location', 'Status', ' ']}
+                columnMapping={columnMapping}
+                columnWidths={{ 'Time Returned': 120, ' ': 80, Title: 300, Barcode: 200, CallNumber: 200, Location: 200, Status: 120 }}
+                columnOverflow={{ ' ': true }}
                 rowMetadata={['id']}
                 contentData={scannedItems}
                 formatter={itemListFormatter}
                 isEmptyMessage="No items have been entered yet."
-                fullWidth
               />
             </div>
           </Pane>
--- Scan.js
+++ Scan.js
@@ -1,9 +1,16 @@
+import _ from 'lodash';
 import React from 'react';
 import PropTypes from 'prop-types';
 import dateFormat from 'dateformat';
-import { SubmissionError, change } from 'redux-form';
-
+import moment from 'moment-timezone'; // eslint-disable-line import/no-extraneous-dependencies
+import Button from '@folio/stripes-components/lib/Button';
+import DropdownMenu from '@folio/stripes-components/lib/DropdownMenu';
+import MenuItem from '@folio/stripes-components/lib/MenuItem';
+import { SubmissionError, change, reset } from 'redux-form';
+import { UncontrolledDropdown } from '@folio/stripes-components/lib/Dropdown';
+import InfoPopover from '@folio/stripes-components/lib/structures/InfoPopover';
 import CheckIn from './CheckIn';
+import formatDateTimePicker from './util';
 
 class Scan extends React.Component {
   static propTypes = {
@@ -18,6 +25,9 @@ class Scan extends React.Component {
         records: PropTypes.arrayOf(PropTypes.object),
       }),
       items: PropTypes.shape({
+        records: PropTypes.arrayOf(PropTypes.object),
+      }),
+      holdings: PropTypes.shape({
         records: PropTypes.arrayOf(PropTypes.object),
       }),
     }),
@@ -35,10 +45,18 @@ class Scan extends React.Component {
         PUT: PropTypes.func,
         reset: PropTypes.func,
       }),
+      holdings: PropTypes.shape({
+        GET: PropTypes.func,
+        reset: PropTypes.func,
+      }),
       scannedItems: PropTypes.shape({
         replace: PropTypes.func,
       }),
     }),
+  };
+
+  static contextTypes = {
+    history: PropTypes.object,
   };
 
   static manifest = Object.freeze({
@@ -64,11 +82,97 @@ class Scan extends React.Component {
       path: 'circulation/loans',
       fetch: false,
     },
+    holdings: {
+      type: 'okapi',
+      records: 'holdingsRecords',
+      path: 'holdings-storage/holdings',
+      accumulate: 'true',
+      fetch: false,
+    },
   });
 
-  constructor(props) {
-    super(props);
+  constructor(props, context) {
+    super(props, context);
+    this.context = context;
+    this.store = props.stripes.store;
+    this.formatDateTime = props.stripes.formatDateTime;
     this.onClickCheckin = this.onClickCheckin.bind(this);
+    this.timezone = this.props.stripes.timezone;
+    this.dateTime = props.stripes.dateTime;
+    this.renderActions = this.renderActions.bind(this);
+    this.showInfo = this.showInfo.bind(this);
+    this.onSessionEnd = this.onSessionEnd.bind(this);
+    this.handleOptionsChange = this.handleOptionsChange.bind(this);
+  }
+
+  handleOptionsChange(key, e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    if (key.action && this[key.action]) {
+      this[key.action](key.loan, e);
+    }
+  }
+
+  showLoanDetails(loan) {
+    this.context.history.push(`/users/view/${loan.userId}?layer=loan&loan=${loan.id}`);
+  }
+
+  showPatronDetails(loan) {
+    const userId = `${_.get(loan, ['patron', 'id'])}`;
+    this.context.history.push(`/users/view/${userId}`);
+  }
+
+  renderActions(loan) {
+    return (
+      <UncontrolledDropdown
+        onSelectItem={this.handleOptionsChange}
+      >
+        <Button data-role="toggle" buttonStyle="hover dropdownActive"><strong>•••</strong></Button>
+        <DropdownMenu data-role="menu" overrideStyle={{ padding: '6px 0' }}>
+          <MenuItem itemMeta={{ loan, action: 'showLoanDetails' }}>
+            <Button buttonStyle="dropdownItem">Loan details</Button>
+          </MenuItem>
+          <MenuItem itemMeta={{ loan, action: 'showPatronDetails' }}>
+            <Button buttonStyle="dropdownItem">Patron details</Button>
+          </MenuItem>
+        </DropdownMenu>
+      </UncontrolledDropdown>
+    );
+  }
+
+  showInfo(loan) {
+    this.systemReturnDate = loan.systemReturnDate;
+    const content =
+    (
+      <div style={{ textAlign: 'left' }}>
+        <div><strong>Processed As:</strong></div>
+        <div>{this.formatDateTime(this.systemReturnDate)}</div>
+        <br />
+        <div><strong>Actual:</strong></div>
+        <div>{this.formatDateTime(new Date())}</div>
+      </div>
+    );
+
+    return (
+      <InfoPopover content={content} />
+    );
+  }
+
+  onSessionEnd() {
+    this.clearResources();
+    this.clearForm('CheckIn');
+  }
+
+  clearForm(formName) {
+    this.store.dispatch(reset(formName));
+  }
+
+  clearResources() {
+    this.props.mutator.scannedItems.replace([]);
+    this.props.mutator.patrons.reset();
+    this.props.mutator.items.reset();
+    this.props.mutator.loans.reset();
   }
 
   onClickCheckin(data) {
@@ -78,9 +182,10 @@ class Scan extends React.Component {
 
     return this.fetchItemByBarcode(data.item.barcode)
       .then(item => this.fetchLoanByItemId(item.id))
-      .then(loan => this.putReturn(loan))
+      .then(loan => this.putReturn(loan, data.item.checkinDate, data.item.checkinTime))
       .then(loan => this.fetchLoanById(loan.id))
       .then(loan => this.fetchPatron(loan))
+      .then(loan => this.fetchHoldings(loan))
       .then(loan => this.addScannedItem(loan))
       .then(() => this.clearField('CheckIn', 'item.barcode'));
   }
@@ -92,7 +197,6 @@ class Scan extends React.Component {
       if (!items.length) {
         throw new SubmissionError({ item: { barcode: 'Item with this barcode does not exist', _error: 'Scan failed' } });
       }
-
       return items[0];
     });
   }
@@ -117,13 +221,20 @@ class Scan extends React.Component {
     });
   }
 
-  putReturn(loan) {
+  putReturn(loan, checkinDate, checkinTime) {
+    // if checkintime == now, convert to UTC Datetime and extract the time string
+    const SystemcheckinTime = (checkinTime === 'now') ?
+      (moment.tz(moment.tz(this.timezone).format(), 'UTC').format().split('T')[1]) : checkinTime;
+    const SystemcheckinDate = (checkinDate === 'today') ? moment.tz(this.timezone).format() : checkinDate;
+    const localDateTime = formatDateTimePicker(SystemcheckinDate, SystemcheckinTime, this.timezone);
+    // Convert the returned local dateTime into UTC to send it to the backend
+    const systemReturnDateUTC = moment.tz(localDateTime, this.timezone).tz('UTC').format();
     Object.assign(loan, {
+      systemReturnDate: systemReturnDateUTC,
       returnDate: dateFormat(new Date(), "yyyy-mm-dd'T'HH:MM:ss'Z'"),
       status: { name: 'Closed' },
       action: 'checkedin',
     });
-
     return this.props.mutator.loans.PUT(loan);
   }
 
@@ -137,6 +248,11 @@ class Scan extends React.Component {
 
       return Object.assign(loan, { patron: patrons[0] });
     });
+  }
+
+  fetchHoldings(loan) {
+    const query = `(id="${loan.userId}")`;
+    return this.props.mutator.holdings.GET({ params: { query } }).then(holdings => Object.assign(loan, { holding: holdings[0] }));
   }
 
   addScannedItem(loan) {
@@ -154,7 +270,17 @@ class Scan extends React.Component {
     return (
       <CheckIn
         submithandler={this.onClickCheckin}
+        renderActions={this.renderActions}
+        showInfo={this.showInfo}
+        onSessionEnd={this.onSessionEnd}
         scannedItems={scannedItems}
+        initialValues={
+          { item:
+            {
+              checkinDate: 'today',
+              checkinTime: 'now',
+            } }
+        }
         {...this.props}
       />
     );
