afb819fdc2131f1e40d17603d885c3e16892ce55
Jeffrey Cherewaty <jeffrey@frontside.io>
2018-07-11 19:00:04 -0500

    Set up BigTest infrastructure

---
+++ karma.conf.js
@@ -0,0 +1,22 @@
+module.exports = (config) => {
+  const testIndex = './bigtest/index.js';
+  const preprocessors = {};
+  preprocessors[`${testIndex}`] = ['webpack'];
+
+  const configuration = {
+    files: [
+      { pattern: testIndex, watched: false },
+    ],
+
+    preprocessors
+  };
+
+  // Set output directory for junit reporter
+  if (config.junitReporter) {
+    configuration.junitReporter = {
+      outputDir: 'artifacts/junit/Karma'
+    };
+  }
+
+  config.set(configuration);
+};
