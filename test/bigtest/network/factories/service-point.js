5d5c5db44146f969df3568f9b2d1668018edccac
Michal Kuklis <michal.kuklis@gmail.com>
2019-02-15 12:49:03 -0500

    Cleanup

--- test/bigtest/network/factories/service-point.js
+++ test/bigtest/network/factories/service-point.js
@@ -1,7 +1,6 @@
 import { Factory, faker } from '@bigtest/mirage';
 
 export default Factory.extend({
-  // id: faker.random.uuid,
   name: faker.company.catchPhrase(),
   code: faker.company.catchPhrase(),
   discoveryDisplayName: faker.company.catchPhrase(),
