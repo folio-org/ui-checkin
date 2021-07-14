9ba63f4e2653d09c5993791172ce2db32c888cf4
Zak Burke <zburke@cornell.edu>
2021-07-14 07:56:18 -0400

    UICHKIN-273 provide `key` when rendering list
    When rendering a list in React, you have to provide a `key` attibute on
    the list item. It's [the
    rule](https://reactjs.org/docs/lists-and-keys.html).
    
    Refs UICHKIN-273

--- src/components/ConfirmStatusModal/ConfirmStatusModal.js
+++ src/components/ConfirmStatusModal/ConfirmStatusModal.js
@@ -71,7 +71,7 @@ class ConfirmStatusModal extends React.Component {
           </Button>}
       </ModalFooter>
     );
-    const messageParts = message.map(m => <p>{m}</p>);
+    const messageParts = message.map((m, i) => <p key={i}>{m}</p>);
 
     return (
       <Modal
