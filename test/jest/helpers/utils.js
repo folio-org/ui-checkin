729fc8e87cc5bcb47a2f2d51c9029d3bcb63c90a
Stanislau Snapok <stanislau_snapok@epam.com>
2022-04-11 15:43:18 +0300

    add useful util

--- test/jest/helpers/utils.js
+++ test/jest/helpers/utils.js
@@ -1,7 +1,21 @@
-/* eslint-disable import/prefer-default-export */
 import {
   screen,
   within,
 } from '@testing-library/react';
 
 export const getById = (id) => within(screen.getByTestId(id));
+
+export const componentPropsCheck = (Component, testId, expectedProps, partialCompare = false) => {
+  const propertiesForCompare = Component.mock.calls
+    .reverse()
+    .find(item => item[0]['data-testid'] === testId);
+
+  const resultExpectedProps = partialCompare
+    ? expect.objectContaining(expectedProps)
+    : {
+      ...expectedProps,
+      'data-testid': testId,
+    };
+
+  expect(propertiesForCompare[0]).toStrictEqual(resultExpectedProps);
+};
