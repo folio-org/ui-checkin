51c4cad806548b8b38faf22adb4fff8f912a7706
Artem Blazhko <artem_blazhko@epam.com>
2022-04-14 16:31:17 +0300

    Cover `PrintButton` component by RTL/jest tests. (#517)

---
+++ src/components/PrintButton/PrintButton.test.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import {
+  render,
+} from '@testing-library/react';
+
+import '../../../test/jest/__mock__';
+
+import {
+  Button,
+} from '@folio/stripes/components';
+import ReactToPrint from 'react-to-print';
+
+import PrintButton from './PrintButton';
+import ComponentToPrint from '../ComponentToPrint';
+
+jest.mock('../ComponentToPrint', () => jest.fn((props) => (
+  <div {...props} />
+)));
+jest.mock('react-to-print', () => jest.fn(({ children, trigger, ...rest }) => (
+  <div {...rest}>
+    {children}
+    {trigger()}
+  </div>
+)));
+
+const defaultProps = {
+  dataSource: {},
+  template: 'testTemplate',
+  onBeforePrint: jest.fn(),
+  onAfterPrint: jest.fn(),
+  children: 'buttonName',
+  specificButtonProp: 'test',
+};
+
+describe('PrintButton', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  beforeEach(() => {
+    render(
+      <PrintButton
+        {...defaultProps}
+      />
+    );
+  });
+
+  it('"ReactToPrint" should be called with correct props', () => {
+    const expectedProps = {
+      trigger: expect.any(Function),
+      content: expect.any(Function),
+      removeAfterPrint: true,
+      onBeforePrint: defaultProps.onBeforePrint,
+      onAfterPrint: defaultProps.onAfterPrint,
+    };
+
+    expect(ReactToPrint).toHaveBeenCalledWith(expect.objectContaining(expectedProps), {});
+  });
+
+  it('"ComponentToPrint" should be called with correct props', () => {
+    const expectedProps = {
+      template: defaultProps.template,
+      dataSource: defaultProps.dataSource,
+    };
+
+    expect(ComponentToPrint).toHaveBeenCalledWith(expect.objectContaining(expectedProps), {});
+  });
+
+  it('"Button" should be called with correct and filtered props', () => {
+    const expectedProps = {
+      specificButtonProp: defaultProps.specificButtonProp,
+      children: defaultProps.children,
+    };
+
+    expect(Button).toHaveBeenCalledWith(expect.objectContaining(expectedProps), {});
+  });
+});
