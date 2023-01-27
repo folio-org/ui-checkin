8d9f0d156f7e1d575139f49dc1e45e20cb989442
Artem Blazhko <artem_blazhko@epam.com>
2023-01-27 11:10:16 +0200

    Cover `SelectItemModal` by jest/RTL tests (#567)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,7 @@
 * Bump major versions of several @folio/stripes-* packages. Refs UICHKIN-369.
 * Remove unneeded `react-redux`. Refs UICHKIN-371.
 * Support `inventory` `13.0` interface version. Refs UICHKIN-366.
+* Cover `SelectItemModal` by jest/RTL tests. UICHKIN-360.
 
 ## [7.2.0] (https://github.com/folio-org/ui-checkin/tree/v7.2.0) (2022-10-20)
 [Full Changelog](https://github.com/folio-org/ui-checkin/compare/v7.1.1...v7.2.0)
---
+++ src/components/SelectItemModal/SelectItemModal.test.js
@@ -0,0 +1,161 @@
+import React from 'react';
+import {
+  render,
+  screen,
+} from '@testing-library/react';
+import '../../../test/jest/__mock__';
+import {
+  Modal,
+  Paneset,
+  Pane,
+  MultiColumnList,
+} from '@folio/stripes/components';
+import SelectItemModal,
+{
+  formatter,
+  COLUMN_NAMES,
+  COLUMN_WIDTHS,
+  COLUMN_MAP,
+  MAX_HEIGHT,
+} from './SelectItemModal';
+import css from './SelectItemModal.css';
+
+describe('SelectItemModal', () => {
+  describe('formatter', () => {
+    const basicItem = {
+      status: {
+        name: 'statusName',
+      },
+      effectiveLocation: {
+        name: 'effectiveLocationName',
+      },
+      materialType: {
+        name: 'materialTypeName',
+      },
+      temporaryLoanType: {
+        name: 'temporaryLoanTypeName',
+      },
+      permanentLoanType: {
+        name: 'permanentLoanTypeName',
+      },
+    };
+
+    describe('when all data exist', () => {
+      it('should return correct "status.name"', () => {
+        expect(formatter.itemStatus(basicItem)).toBe(basicItem.status.name);
+      });
+
+      it('should return correct "effectiveLocation.name"', () => {
+        expect(formatter.location(basicItem)).toBe(basicItem.effectiveLocation.name);
+      });
+
+      it('should return correct "materialType.name"', () => {
+        expect(formatter.materialType(basicItem)).toBe(basicItem.materialType.name);
+      });
+
+      it('should return correct "temporaryLoanType.name"', () => {
+        expect(formatter.loanType(basicItem)).toBe(basicItem.temporaryLoanType.name);
+      });
+    });
+
+    describe('when "effectiveLocation" does not have data', () => {
+      const item = {
+        ...basicItem,
+        effectiveLocation: {},
+      };
+
+      it('should return empty string for "location"', () => {
+        expect(formatter.location(item)).toBe('');
+      });
+    });
+
+    describe('when "temporaryLoanType" does not have data', () => {
+      const item = {
+        ...basicItem,
+        temporaryLoanType: {},
+      };
+
+      it('should return empty "permanentLoanType.name"', () => {
+        expect(formatter.loanType(item)).toBe(item.permanentLoanType.name);
+      });
+    });
+
+    describe('when "temporaryLoanType" and "permanentLoanType" do not have data', () => {
+      const item = {
+        ...basicItem,
+        temporaryLoanType: {},
+        permanentLoanType: {},
+      };
+
+      it('should return empty string for "loanType"', () => {
+        expect(formatter.loanType(item)).toBe('');
+      });
+    });
+  });
+
+  describe('component', () => {
+    const props = {
+      checkedinItems: [],
+      onClose: jest.fn(),
+      onSelectItem: jest.fn(),
+    };
+    const modalTestId = 'selectItemModal';
+
+    beforeAll(() => {
+      render(
+        <SelectItemModal {...props} />
+      );
+    });
+
+    it('should render modal window', () => {
+      expect(screen.getByTestId(modalTestId)).toBeVisible();
+    });
+
+    it('should trigger "Modal" with correct props', () => {
+      const expectedProps = {
+        'data-test-select-item-modal': true,
+        open: true,
+        dismissible: true,
+        onClose: props.onClose,
+        contentClass: css.content,
+      };
+
+      expect(Modal).toHaveBeenCalledWith(expect.objectContaining(expectedProps), {});
+    });
+
+    it('should trigger "Paneset" with correct props', () => {
+      const expectedProps = {
+        id: 'itemsDialog',
+        isRoot: true,
+        static: true,
+      };
+
+      expect(Paneset).toHaveBeenCalledWith(expect.objectContaining(expectedProps), {});
+    });
+
+    it('should trigger "Pane" with correct props', () => {
+      const expectedProps = {
+        id: 'check-in-items-list',
+        defaultWidth: 'fill',
+      };
+
+      expect(Pane).toHaveBeenCalledWith(expect.objectContaining(expectedProps), {});
+    });
+
+    it('should trigger "MultiColumnList" with correct props', () => {
+      const expectedProps = {
+        id: 'items-list',
+        interactive: true,
+        contentData: props.checkedinItems,
+        visibleColumns: COLUMN_NAMES,
+        columnMapping: COLUMN_MAP,
+        columnWidths: COLUMN_WIDTHS,
+        formatter,
+        maxHeight: MAX_HEIGHT,
+        onRowClick: props.onSelectItem,
+      };
+
+      expect(MultiColumnList).toHaveBeenCalledWith(expect.objectContaining(expectedProps), {});
+    });
+  });
+});
