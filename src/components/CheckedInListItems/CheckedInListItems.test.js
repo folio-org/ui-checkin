c617297bb0d52b654f60e2cade20979255ddd4b7
Artem Blazhko <artem_blazhko@epam.com>
2023-03-06 12:11:08 +0200

    UICHKIN-361: UI tests replacement with RTL/Jest for CheckedInListItems (#575)

--- src/components/CheckedInListItems/CheckedInListItems.test.js
+++ src/components/CheckedInListItems/CheckedInListItems.test.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import {
   render,
   screen,
@@ -6,11 +5,25 @@ import {
 
 import '../../../test/jest/__mock__';
 
-import CheckedInListItems from './CheckedInListItems';
+import {
+  Icon,
+  MultiColumnList,
+} from '@folio/stripes/components';
+import { effectiveCallNumber } from '@folio/stripes/util';
+
+import ReturnedTime from './components/ReturnedTime';
+import CheckedInListItems, {
+  COLUMNS_NAME,
+  visibleColumns,
+  columnMapping,
+  columnWidths,
+  getItemListFormatter,
+} from './CheckedInListItems';
 
 const testIds = {
   listItemsLoading: 'listItemsLoading',
   listItems: 'listItems',
+  returnedTime: 'returnedTime',
 };
 
 const loading = false;
@@ -24,41 +37,171 @@ const initialProps = {
   renderActions,
 };
 
+jest.mock('@folio/stripes/util', () => ({
+  effectiveCallNumber: jest.fn(),
+}));
+jest.mock('./components/ReturnedTime', () => jest.fn(() => null));
+
 describe('CheckedInListItems', () => {
-  describe('with loading false', () => {
-    beforeEach(() => {
-      render(
-        <CheckedInListItems
-          {...initialProps}
-        />
-      );
+  describe('component', () => {
+    describe('with loading false', () => {
+      beforeEach(() => {
+        render(
+          <CheckedInListItems
+            {...initialProps}
+          />
+        );
+      });
+
+      it('should not render loading', () => {
+        expect(screen.queryByTestId(testIds.listItemsLoading)).not.toBeInTheDocument();
+      });
+
+      it('should render list', () => {
+        expect(screen.getByTestId(testIds.listItems)).toBeVisible();
+      });
+
+      it('should trigger "MultiColumnList" with correct props', () => {
+        const expectedProps = {
+          id: 'list-items-checked-in',
+          fullWidth: true,
+          columnOverflow: {
+            ' ': true,
+          },
+          rowMetadata: ['id'],
+          interactive: false,
+          contentData: scannedItems,
+          visibleColumns,
+          columnMapping,
+          columnWidths,
+        };
+
+        expect(MultiColumnList).toHaveBeenCalledWith(expect.objectContaining(expectedProps), {});
+      });
     });
 
-    it('should not render loading', () => {
-      expect(screen.queryByTestId(testIds.listItemsLoading)).not.toBeInTheDocument();
-    });
+    describe('with loading true', () => {
+      const defaultProps = {
+        ...initialProps,
+        loading: true,
+      };
 
-    it('should render list', () => {
-      expect(screen.getByTestId(testIds.listItems)).toBeVisible();
+      beforeEach(() => {
+        render(
+          <CheckedInListItems
+            {...defaultProps}
+          />
+        );
+      });
+
+      it('should render loading', () => {
+        expect(screen.getByTestId(testIds.listItemsLoading)).toBeVisible();
+      });
     });
   });
 
-  describe('with loading true', () => {
-    const defaultProps = {
-      ...initialProps,
-      loading: true,
+  describe('getItemListFormatter', () => {
+    const basicLoan = {
+      item: {
+        title: 'title',
+        barcode: 'barcode',
+        location: {
+          name: 'location',
+        },
+        materialType: {
+          name: 'materialType',
+        },
+        status: {
+          name: 'status',
+        },
+        inTransitDestinationServicePoint: {
+          name: 'inTransitDestinationServicePoint',
+        },
+      },
+      inHouseUse: true,
     };
 
-    beforeEach(() => {
-      render(
-        <CheckedInListItems
-          {...defaultProps}
-        />
-      );
+    it('should trigger "ReturnedTime" with correct props', () => {
+      const returnedTime = getItemListFormatter(mutator, renderActions)[COLUMNS_NAME.TIME_RETURNED](basicLoan);
+      const expectedProps = {
+        mutator,
+        loan: basicLoan,
+      };
+
+      render(returnedTime);
+
+      expect(ReturnedTime).toHaveBeenCalledWith(expectedProps, {});
     });
 
-    it('should render loading', () => {
-      expect(screen.getByTestId(testIds.listItemsLoading)).toBeVisible();
+    it('should return title', () => {
+      const title = getItemListFormatter(mutator, renderActions)[COLUMNS_NAME.TITLE](basicLoan);
+
+      expect(title).toBe(`${basicLoan.item.title} (${basicLoan.item.materialType.name})`);
+    });
+
+    it('should return barcode', () => {
+      const barcode = getItemListFormatter(mutator, renderActions)[COLUMNS_NAME.BARCODE](basicLoan);
+
+      expect(barcode).toBe(basicLoan.item.barcode);
+    });
+
+    it('should return location', () => {
+      const location = getItemListFormatter(mutator, renderActions)[COLUMNS_NAME.LOCATION](basicLoan);
+
+      expect(location).toBe(basicLoan.item.location.name);
+    });
+
+    it('should trigger "Icon" with correct props', () => {
+      const inHouseUse = getItemListFormatter(mutator, renderActions)[COLUMNS_NAME.IN_HOUSE_USE](basicLoan);
+      const expectedProps = {
+        icon: 'house',
+        iconClassName: 'houseIcon',
+      };
+
+      render(inHouseUse);
+
+      expect(Icon).toHaveBeenCalledWith(expectedProps, {});
+    });
+
+    it('should return empty string as "In house use" column', () => {
+      const loan = {
+        ...basicLoan,
+        inHouseUse: false,
+      };
+      const inHouseUse = getItemListFormatter(mutator, renderActions)[COLUMNS_NAME.IN_HOUSE_USE](loan);
+
+      expect(inHouseUse).toBe('');
+    });
+
+    it('should return status with service point', () => {
+      const status = getItemListFormatter(mutator, renderActions)[COLUMNS_NAME.STATUS](basicLoan);
+
+      expect(status).toBe(`${basicLoan.item.status.name} - ${basicLoan.item.inTransitDestinationServicePoint.name}`);
+    });
+
+    it('should return status without service point', () => {
+      const loan = {
+        ...basicLoan,
+        item: {
+          ...basicLoan.item,
+          inTransitDestinationServicePoint: {},
+        },
+      };
+      const status = getItemListFormatter(mutator, renderActions)[COLUMNS_NAME.STATUS](loan);
+
+      expect(status).toBe(basicLoan.item.status.name);
+    });
+
+    it('should trigger "effectiveCallNumber" with correct arguments', () => {
+      getItemListFormatter(mutator, renderActions)[COLUMNS_NAME.EFFECTIVE_CALL_NUMBER](basicLoan);
+
+      expect(effectiveCallNumber).toHaveBeenCalledWith(basicLoan);
+    });
+
+    it('should trigger "renderActions" with correct arguments', () => {
+      getItemListFormatter(mutator, renderActions)[COLUMNS_NAME.ACTION](basicLoan);
+
+      expect(renderActions).toHaveBeenCalledWith(basicLoan);
     });
   });
 });
---
+++ src/components/CheckedInListItems/components/ReturnedTime/ReturnedTime.test.js
@@ -0,0 +1,100 @@
+import {
+  render,
+  screen,
+  cleanup,
+} from '@testing-library/react';
+
+import '../../../../../test/jest/__mock__';
+
+import ReturnedTime from './ReturnedTime';
+import FeesFinesOwnedStatus from '../FeesFinesOwnedStatus';
+
+const testIds = {
+  returnedDate: 'returnedDate',
+};
+const messageIds = {
+  processedAs: 'ui-checkin.processedAs',
+  actualTime: 'ui-checkin.actual',
+};
+
+jest.mock('../FeesFinesOwnedStatus', () => jest.fn(() => null));
+
+describe('ReturnedTime', () => {
+  const basicProps = {
+    mutator: {},
+    loan: {
+      returnDate: '2023-03-02T10:51:22.527Z',
+      id: 'loanId',
+      userId: 'userId',
+      itemId: 'itemId',
+    },
+  };
+
+  describe('when all data provided', () => {
+    beforeEach(() => {
+      render(
+        <ReturnedTime
+          {...basicProps}
+        />
+      );
+    });
+
+    afterEach(cleanup);
+
+    it('should render returned date into the document', () => {
+      const returnedDate = screen.getByTestId(testIds.returnedDate);
+
+      expect(returnedDate).toBeInTheDocument();
+    });
+
+    it('should render "Processes as" label', () => {
+      const processedAsLabel = screen.getByText(messageIds.processedAs);
+
+      expect(processedAsLabel).toBeInTheDocument();
+    });
+
+    it('should render "Actual" label', () => {
+      const actualTimeLabel = screen.getByText(messageIds.actualTime);
+
+      expect(actualTimeLabel).toBeInTheDocument();
+    });
+
+    it('should trigger "FeesFinesOwnedStatus" with correct props', () => {
+      const expectedProps = {
+        userId: basicProps.loan.userId,
+        itemId: basicProps.loan.itemId,
+        loanId: basicProps.loan.id,
+        mutator: basicProps.mutator,
+      };
+
+      expect(FeesFinesOwnedStatus).toHaveBeenCalledWith(expectedProps, {});
+    });
+  });
+
+  describe('when there are no "returnDate", "userId", "itemId"', () => {
+    const props = {
+      ...basicProps,
+      loan: {},
+    };
+
+    beforeEach(() => {
+      FeesFinesOwnedStatus.mockClear();
+
+      render(
+        <ReturnedTime
+          {...props}
+        />
+      );
+    });
+
+    it('should not render returned date into the document', () => {
+      const returnedDate = screen.queryByTestId(testIds.returnedDate);
+
+      expect(returnedDate).not.toBeInTheDocument();
+    });
+
+    it('should not trigger "FeesFinesOwnedStatus"', () => {
+      expect(FeesFinesOwnedStatus).not.toHaveBeenCalled();
+    });
+  });
+});
