cabdd12d9c28001d5480480eb1128cbc753a5f97
Dmitriy-Litvinenko <Dmitriy_Litvinenko@epam.com>
2023-08-24 12:54:54 +0300

    UICHKIN-388: Remove bigtest

--- test/bigtest/network/factories/staff-slip.js
+++
@@ -1,9 +0,0 @@
-import { Factory } from 'miragejs';
-import faker from 'faker';
-
-export default Factory.extend({
-  id: faker.random.uuid,
-  name: faker.random.arrayElement(['Hold', 'Transit']),
-  active: true,
-  template: '<p>{{Barcode}}</p>',
-});
--- test/bigtest/network/scenarios/circulation-requests.js
+++
@@ -1,6 +0,0 @@
-export default server => {
-  server.get('/circulation/requests', {
-    requests : [],
-    totalRecords : 0,
-  });
-};
