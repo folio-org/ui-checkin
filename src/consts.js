70f895bd9e52b91f28c4a0248cef2c0f60da53e1
Dmytro Melnyshyn <dmytro_melnyshyn@epam.com>
2025-10-15 17:45:43 +0300

    UICHKIN-495: Use loan.isDcb instead of loan.borrower.lastName to determine whether the to hide fee fine buttons.

--- src/consts.js
+++ src/consts.js
@@ -136,12 +136,6 @@ export const CIRCULATION_BFF_INVENTORY_INTERFACE_ERROR = `Required okapi interfa
 export const PAGE_AMOUNT = 100;
 export const MAX_RECORDS = '1000';
 
-export const DCB_USER_LASTNAME = 'DcbSystem';
-
-export const DCB_USER = {
-  lastName: DCB_USER_LASTNAME,
-};
-
 export const DCB_INSTANCE_ID = '9d1b77e4-f02e-4b7f-b296-3f2042ddac54';
 export const DCB_HOLDINGS_RECORD_ID = '10cd3a5a-d36f-4c7a-bc4f-e1ae3cf820c9';
 
